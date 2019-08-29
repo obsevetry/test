e309ec9611695c5a2e2b3330b5e600aabe2be9e4
Гоцко Максим <gotskomp@comfy.ua>
2019-08-29 18:34:56 +0300

    ывфа

---
+++ 20190830/20190830_1/20190830_1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _20190830_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите первую строку для сравнения");
+            string firstSrting = Console.ReadLine();
+
+            Console.WriteLine("Введите вторую строку для сравнения");
+            string secondSrting = Console.ReadLine();
+
+            string strResult = "Строки cовпадают";
+
+            // Если длина строк отличается, дальше можно не проверять
+            if (firstSrting.Length == secondSrting.Length)
+            {
+                for (int i = 0; i < firstSrting.Length; i++)
+                {
+
+                    if (firstSrting[i] != secondSrting[i])
+                    {
+                        strResult = "Строки НЕ cовпадают";
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                strResult = "Строки НЕ cовпадают";
+            }
+
+            Console.WriteLine("__________________");
+            Console.WriteLine(strResult);
+            Console.ReadKey();
+
+        }
+    }
+}
