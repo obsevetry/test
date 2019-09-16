bdc836509e0a9ef89cf9a983342727a586af2933
Гоцко Максим <gotskomp@comfy.ua>
2019-09-16 16:15:02 +0300

    фикс

---
+++ 18082019_1/18082019_1/20190916_3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _20190916_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
