48f0dc37fbb9661401014bdcd0c57dba6d86a1d5
obsevetry@live.com <obsevetry@live.com>
2019-09-21 00:52:00 +0300

    abrc

--- 18082019_1/18082019_1/20190916_3/Program.cs
+++ 18082019_1/18082019_1/20190916_3/Program.cs
@@ -7,8 +7,88 @@ namespace _20190916_3
 {
     class Program
     {
+
+        public static int GetMinMaxNumber(int counter, int maxNumberSupport, int minNumberSupport, out int maxNumber, out int minNumber)
+        {
+
+            if (counter == 0)
+            {
+                maxNumber = maxNumberSupport;
+                minNumber = minNumberSupport;
+                return 1;
+            }
+            else
+            {
+
+
+                string Result = Console.ReadLine();
+
+                if (Result == "=")
+                {
+                    maxNumber = maxNumberSupport;
+                    minNumber = minNumberSupport;
+                    return 1;
+                }
+
+                int currentNumber = 0;
+
+                int.TryParse(Result, out currentNumber);
+
+                int currentMax = 0;
+
+                if (currentNumber > maxNumberSupport)
+                {
+                    currentMax = currentNumber;
+                }
+                else
+                {
+                    currentMax = maxNumberSupport;
+                }
+
+                int currentMin = 0;
+
+                if (currentNumber < minNumberSupport)
+                {
+                    currentMin = currentNumber;
+                }
+                else
+                {
+                    currentMin = minNumberSupport;
+                }
+
+                maxNumber = currentMax;
+                minNumber = currentMin;
+
+                return GetMinMaxNumber(counter - 1, currentMax, currentMin, out maxNumber, out minNumber);
+            }
+
+        }
+
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Введите количество вводимых чисел (чтобы закончить цикл введите знак '='):");
+
+            int counter = 0;
+
+            if (int.TryParse(Console.ReadLine(), out counter) != true)
+            {
+                Console.WriteLine("Введены некорректные данные! Необходимо вводить только целые числа!");
+            }
+
+            int maxNumber = 0;
+            int minNumber = 0;
+
+            int maxNumberSupport = int.MinValue;
+            int minNumberSupport = int.MaxValue;
+
+            GetMinMaxNumber(counter, maxNumberSupport, minNumberSupport, out maxNumber, out minNumber);
+
+            Console.WriteLine("_______________________________________");
+            Console.WriteLine("Минимальное число {0}", minNumber);
+            Console.WriteLine("Максимальное число {0}", maxNumber);
+
+
         }
     }
 }
