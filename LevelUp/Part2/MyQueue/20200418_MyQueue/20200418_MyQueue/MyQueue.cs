6f3852578aa51f1a7734bb3d365a2545bb307c1b
obsevetry@live.com <obsevetry@live.com>
2020-04-18 22:03:57 +0300

    MyQueue

---
+++ LevelUp/Part2/MyQueue/20200418_MyQueue/20200418_MyQueue/MyQueue.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20200418_MyQueue
+{
+    public class MyQueue : IEnumerable<string>
+    {
+        private string [] _array;
+        private int _head; // First valid element in the queue
+        private int _tail; // Last valid element in the queue
+        private int _size;
+
+        private const int _GrowFactor = 2;
+
+        public int Count
+        {
+            get { return _size; }
+        }
+
+        public int Length
+        {
+            get { return _array.Length; }
+        }
+        public MyQueue(int capacity)
+        {
+            if (capacity < 0)
+                throw new NotImplementedException();
+
+            _array = new string[capacity];
+            _head = 0;
+            _tail = 0;
+            _size = 0;
+        }
+
+        public void Enqueue(string item)
+        {
+            if (_size == _array.Length)
+            {
+                int newcapacity = _array.Length * _GrowFactor;
+                SetCapacity(newcapacity);
+            }
+
+            _array[_tail] = item;
+            _tail = (_tail + 1) % _array.Length;
+            _size++;
+        }
+
+        public string Dequeue()
+        {
+            if (_size == 0)
+                throw new NotImplementedException(); ;
+
+            string removed = _array[_head];
+            _array[_head] = default;
+            _head = (_head + 1) % _array.Length;
+            _size--;
+            return removed;
+        }
+
+        private void SetCapacity(int capacity)
+        {
+            string[] newArray = new string[capacity];
+            if (_size > 0)
+            {
+                if (_head < _tail)
+                {
+                    Array.Copy(_array, _head, newArray, 0, _size);
+                }
+                else
+                {
+                    Array.Copy(_array, _head, newArray, 0, _array.Length - _head);
+                    Array.Copy(_array, 0, newArray, _array.Length - _head, _tail);
+                }
+            }
+
+            _array = newArray;
+            _head = 0;
+            _tail = (_size == capacity) ? 0 : _size;
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            return ((IEnumerable<string>)_array).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<string>)_array).GetEnumerator();
+        }
+    }
+}
---
+++ LevelUp/Part2/MyQueue/20200418_MyQueue/20200418_MyQueue/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20200418_MyQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyQueue myQueue = new MyQueue(10);
+
+            // Заполнение стека
+            for (int i = 0; i < 10; i++)
+            {
+                myQueue.Enqueue(i.ToString());
+            }
+
+            Print(myQueue);
+
+            Console.WriteLine("Извлекли {0}", myQueue.Dequeue());
+            Console.WriteLine("Извлекли {0}", myQueue.Dequeue());
+            Console.WriteLine("Извлекли {0}", myQueue.Dequeue());
+
+            Console.WriteLine("Length {0}", myQueue.Length);
+
+            myQueue.Enqueue("a");
+            myQueue.Enqueue("b");
+
+            Console.WriteLine("Length {0}", myQueue.Length);
+
+            Print(myQueue);
+
+            Console.ReadKey();
+
+        }
+
+        private static void Print(MyQueue myQueue)
+        {
+            foreach (string item in myQueue)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
